236213863fc16f706e8a98370a52a4f8acf59204
tk-codelab <tk.codelab@gmail.com>
2021-10-31 11:58:58 +0100

    Chore: initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Why this project?
+
+I need to learn Typescript. Found these exercises to be very helpful!
+
+https://typescript-exercises.github.io/
+
+I've updated the links to the documentation, because the links referred to in the exercsises were outdated.
+
+For each exercise I've described in my own words what the key learning point was.
---
+++ one.ts
@@ -0,0 +1,46 @@
+/* Exercise:
+
+    Given the data, define the interface "User" and use it accordingly.
+
+*/
+
+export interface User {
+    name: string;
+    age: number;
+    occupation: string;
+}
+
+export const users: User[] = [
+    {
+        name: 'Max Mustermann',
+        age: 25,
+        occupation: 'Chimney sweep'
+    },
+    {
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut'
+    }
+];
+
+export function logPerson(user: User) {
+    console.log(` - ${user.name}, ${user.age}`);
+}
+
+console.log('Users:');
+users.forEach(logPerson);
+
+
+/* In case if you are stuck:
+
+// https://www.typescriptlang.org/docs/handbook/2/everyday-types.html#interfaces
+*/
+
+/* In my own words: what is an interface?
+Interface is a way to build a type. (Type is another way, but Interface is preferred)
+It's a way to name an object type.
+You're saying: "If you're going to make an object User, this is the form object User should take. What is in it, and what format is used for it"
+
+What's the difference then between Interface and Type?
+ "Almost all features of an interface are available in type, the key distinction is that a type cannot be re-opened to add new properties vs an interface which is always extendable."
+*/
